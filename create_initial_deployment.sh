291a7b5ed1bdeab7704a60b173b97cc9bcf475b7
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2020-12-29 16:41:55 -0800

    Create create_initial_deployment.sh
    Script to create the initial deployment

---
+++ create_initial_deployment.sh
@@ -0,0 +1,10 @@
+ #!/bin/sh
+ 
+aws eks --region us-west-2 update-kubeconfig --name capstonecluster
+kubectl config use-context arn:aws:eks:us-west-2:556332433231:cluster/capstonecluster
+kubectl apply -f deployment.yml
+sleep 1m
+kubectl get nodes
+kubectl get deployment                      
+kubectl get pod -o wide
+kubectl get service/capstone-project-cloud-devops
