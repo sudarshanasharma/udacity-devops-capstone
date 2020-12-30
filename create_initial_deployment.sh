36fd9da11045be922b81c9a90d131423124dc23a
sudarshanasharma <72416797+sudarshanasharma@users.noreply.github.com>
2020-12-30 12:18:47 -0800

    Update create_initial_deployment.sh
    give more time for the nodes to come up

--- create_initial_deployment.sh
+++ create_initial_deployment.sh
@@ -3,7 +3,7 @@
 aws eks --region us-west-2 update-kubeconfig --name capstonecluster
 kubectl config use-context arn:aws:eks:us-west-2:556332433231:cluster/capstonecluster
 kubectl apply -f deployment.yml --record
-sleep 1m
+sleep 2m
 kubectl get nodes
 kubectl get deployment                      
 kubectl get pod -o wide
